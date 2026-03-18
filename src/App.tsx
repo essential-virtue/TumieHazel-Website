5195392a9fe2a35dd07a1d854b1801488b464ae0
essential-virtue <info@essentialvirtue.co.za>
2026-03-18 21:04:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import Index from "./pages/Index";
 import BookAppointment from "./pages/BookAppointment";
 import Gallery from "./pages/Gallery";
 import SundaySpecials from "./pages/SundaySpecials";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Services from "./pages/Services";
 import ServiceDetail from "./pages/ServiceDetail";
 import Packages from "./pages/Packages";
 import NotFound from "./pages/NotFound";
@@ -21,12 +23,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/book" element={<BookAppointment />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/sunday-specials" element={<SundaySpecials />} />
           <Route path="/packages" element={<Packages />} />
+          <Route path="/services" element={<Services />} />
           <Route path="/services/:slug" element={<ServiceDetail />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
